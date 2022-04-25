212695aaeefc42c2c2faf56a47def3b0a20b3f98
mateusscarin <mateus.scarin@gmail.com>
2022-04-25 15:40:44 -0300

    FlowScoped pt3

---
+++ maratonaJsf/src/main/java/br/com/maratonajsf/flowbuilder/NewPendenciesFlowBuilder.java
@@ -0,0 +1,24 @@
+package br.com.maratonajsf.flowbuilder;
+
+import java.io.Serializable;
+import javax.enterprise.inject.Produces;
+import javax.faces.flow.Flow;
+import javax.faces.flow.builder.FlowBuilder;
+import javax.faces.flow.builder.FlowBuilderParameter;
+import javax.faces.flow.builder.FlowDefinition;
+
+public class NewPendenciesFlowBuilder implements Serializable {
+
+    @Produces
+    @FlowDefinition
+    public Flow defineFlow(@FlowBuilderParameter FlowBuilder flowBuilder) {
+        String flowId = "newpendencies";
+        flowBuilder.id("", flowId);
+        flowBuilder.viewNode(flowId, "/newpendencies/newpendencies.xhtml").markAsStartNode();
+        flowBuilder.returnNode("goToNewRegistration3").fromOutcome("/newregistration/newregistration3.xhtml");
+        flowBuilder.returnNode("exitToNewInicio").fromOutcome("/newregistration/newregistration.xhtml");
+        flowBuilder.inboundParameter("userName", "#{testeFlowBuilderNestedBean.userName}");
+        flowBuilder.inboundParameter("userSurname", "#{testeFlowBuilderNestedBean.userSurname}");
+        return flowBuilder.getFlow();
+    }
+}
