a6179d27f9e0264e54aa4273538f9052aa53805a
mateusscarin <mateus.scarin@gmail.com>
2022-04-27 15:03:49 -0300

    Comunicação pt5 - Add atributos aos componentes com f:attribute

---
+++ maratonaJsf/src/main/java/br/com/maratonajsf/bean/comunicacao/ComunicacaoTeste4Bean.java
@@ -0,0 +1,43 @@
+package br.com.maratonajsf.bean.comunicacao;
+
+import br.com.maratonajsf.model.Estudante;
+import java.io.Serializable;
+import javax.faces.event.ActionEvent;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class ComunicacaoTeste4Bean implements Serializable{
+       
+    private String buttonName = "Nome do botao bean";
+    private Estudante estudante = new Estudante();
+    
+    public void execute(ActionEvent event){
+        String nome = (String) event.getComponent().getAttributes().get("nome");
+        String adjetivo = (String) event.getComponent().getAttributes().get("adjetivo");
+        Estudante estudante = (Estudante) event.getComponent().getAttributes().get("estudante");
+        String test = (String) event.getComponent().getAttributes().get("test");
+        System.out.println(nome);
+        System.out.println(adjetivo);
+        System.out.println(estudante.getNome());
+        System.out.println(test);
+    }
+
+    public String getButtonName() {
+        return buttonName;
+    }
+
+    public void setButtonName(String buttonName) {
+        this.buttonName = buttonName;
+    }
+
+    public Estudante getEstudante() {
+        return estudante;
+    }
+
+    public void setEstudante(Estudante estudante) {
+        this.estudante = estudante;
+    }
+    
+}
