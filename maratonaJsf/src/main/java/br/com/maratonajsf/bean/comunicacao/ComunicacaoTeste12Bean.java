02ec01e205c6f0bdd4c1c8bf369ac6f84e3c2a03
mateusscarin <mateus.scarin@gmail.com>
2022-04-29 11:11:14 -0300

    Ciclo de vida do JSF - Exemplo immediate=true

--- maratonaJsf/src/main/java/br/com/maratonajsf/bean/comunicacao/ComunicacaoTeste12Bean.java
+++ maratonaJsf/src/main/java/br/com/maratonajsf/bean/comunicacao/ComunicacaoTeste12Bean.java
@@ -11,9 +11,17 @@ import javax.inject.Named;
 public class ComunicacaoTeste12Bean implements Serializable {
 
     private Estudante estudante;
+    private Estudante estudante2 = new Estudante();
 
     public void init() {
+        System.out.println("entrou");
         estudante = (Estudante) ApplicationMapUtil.getValueFromApplicationMap("estudante");
+    }
+
+    public String voltar() {
+        System.out.println(estudante2.getNome());
+        System.out.println(estudante2.getSobrenome());
+        return "comunicacao11?faces-redirect=true";
     }
 
     public Estudante getEstudante() {
@@ -22,6 +30,14 @@ public class ComunicacaoTeste12Bean implements Serializable {
 
     public void setEstudante(Estudante estudante) {
         this.estudante = estudante;
+    }
+
+    public Estudante getEstudante2() {
+        return estudante2;
+    }
+
+    public void setEstudante2(Estudante estudante2) {
+        this.estudante2 = estudante2;
     }
 
 }
