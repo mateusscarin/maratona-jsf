c87cbc6d83c963d17c348340af453cbd57cddf13
Mateus-Scarin <mateus.scarin@gmail.com>
2022-04-30 22:45:07 -0300

    Validação customizada com Validators pt2

--- maratonaJsf/src/main/java/br/com/maratonajsf/validators/Validators.java
+++ maratonaJsf/src/main/java/br/com/maratonajsf/validators/Validators.java
@@ -1,29 +1,29 @@
 package br.com.maratonajsf.validators;
 
 import br.com.maratonajsf.bean.login.LoginBean;
+import java.io.Serializable;
 import static java.util.Arrays.asList;
 import java.util.List;
+import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
-import javax.faces.validator.FacesValidator;
-import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 import javax.inject.Inject;
+import javax.inject.Named;
 
-@FacesValidator
-public class DuplicatedEmailValidator implements Validator{
+@Named
+@RequestScoped
+public class Validators implements Serializable {
 
     private List<String> emailsBD = asList("mateus@gmail.com", "mateus.scarin@gestaotec.com", "scarin@fatec.com");
-    
     @Inject
     private LoginBean loginBean;
-    
-    @Override
-    public void validate(FacesContext facesContext, UIComponent uiComponent, Object o) throws ValidatorException {
+
+    public void validateDuplicatedEmail(FacesContext facesContext, UIComponent uiComponent, Object o) throws ValidatorException {
         System.out.println(loginBean.getLanguage());
         String email = (String) o;
-        if(emailsBD.contains(email)){
+        if (emailsBD.contains(email)) {
             FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "O email já foi cadastrado", "");
             throw new ValidatorException(message);
         }
